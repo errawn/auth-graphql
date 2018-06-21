d69a8c99e97b6bcbdfdc3be075c5792fc6eb3ca7
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-21 16:03:49 +0800

    refrain from using passport-js. used jwt insted

--- schema/mutations.js
+++ schema/mutations.js
@@ -1,23 +1,41 @@
 const graphql = require('graphql')
 const {
 	GraphQLObjectType,
-	GraphQLString
+	GraphQLString,
+	GraphQLNonNull
 } = graphql
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserType = require('./types/user_type')
-const AuthService = require('../services/auth')
 
 const mutation = new GraphQLObjectType({
 	name: 'Mutation',
 	fields: {
-		signup: {
+		auth: {
 			type: UserType,
 			args: {
-				email: { type: GraphQLString },
-				password: { type: GraphQLString }
+				email: { type: new GraphQLNonNull(GraphQLString) },
+				password: { type: new GraphQLNonNull(GraphQLString) }
 			},
-			resolve(parentValue, { email, password }, request) {
-				return AuthService.signup({ email, password }, request)
+			// params: parentValue, args, context
+			resolve(parentValue, { email, password }, { model, SECRET }) {
+				return new Promise((resolve, reject) => {
+					model.User.find({ where: { email } })
+						.then(user => {
+							if (!user)
+								return reject('Invalid Credentials')
+							if (!bcrypt.compareSync(password, user.password))
+								return reject('Invalid Credentials')
+
+							const token = jwt.sign({ user: { id: user.id, email: user.email } }, SECRET, { expiresIn: '1yr' })
+							user.token = token
+							resolve(user)
+
+						})
+						.catch(error => reject(error))
+				})
+
 			}
 		}
 	}
