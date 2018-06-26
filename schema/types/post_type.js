c5471bfaffd993de15920e03ceeffc6d5817d81b
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-26 11:00:55 +0800

    get user object along with the lists of posts

--- schema/types/post_type.js
+++ schema/types/post_type.js
@@ -7,13 +7,29 @@ const {
 	GraphQLNonNull
 } = graphql
 
+const UserType = require('./user_type')
+
 const PostType = new GraphQLObjectType({
 	name: 'Post',
 	fields: () => ({
 		id: { type: GraphQLID },
 		title: { type: GraphQLString },
 		body: { type: GraphQLString },
-		userId: { type: GraphQLInt }
+		userId: { type: GraphQLInt },
+		user: {
+			type: UserType, // we are not using new GraphQLList(UserType) becase user is not an array!
+			resolve(parentValue, args, { model, user }) {
+				return new Promise((resolve, reject) => {
+					model.User.find({ where: { id: parentValue.userId } })
+						.then(user => {
+							if (!user) return reject('No user')
+							resolve(user)
+						})
+						.catch(error => reject(error))
+				})
+				
+			}
+		}
 	})
 })
 
